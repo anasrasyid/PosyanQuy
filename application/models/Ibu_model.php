286bb6d2ad36f1e74caf08c86f341d99bee55ffc
Nisa1999 <48039590+Nisa1999@users.noreply.github.com>
2020-04-18 22:25:50 +0700

    Update Ibu_model.php
    function search masih kosong

--- application/models/Ibu_model.php
+++ application/models/Ibu_model.php
@@ -3,25 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Ibu_model extends CI_Model {
 
-	public function get($id) {
-		
+	public function get_ibu($id_ibu) {
+		$this->db->where('id',$id_ibu);
+		return $this->db->get('ibu')->row_array();
 	}
     
-    public function get_all() {
-		
+    public function get_all_ibu() {
+		return $this->db->get('ibu')->result_array();
 	}
     
     
     public function create($data) {
-		
+		return $this->db->insert('ibu',$data);
 	}
     
-    public function update($id, $data) {
-		
+    public function update($id_ibu, $data) {
+		$this->db->where('id',$id_ibu);
+		return $this->db->update('ibu',$data);
 	}
     
-    public function delete($id) {
-		
+    public function delete($id_ibu) {
+		$this->db->where('id',$id_ibu);
+		return $this->db->delete('ibu');
 	}
     
     public function search($keyword) {
