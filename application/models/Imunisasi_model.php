0c9a123c847f00c1b39d5f7b57590dcb6353803f
Nisa1999 <48039590+Nisa1999@users.noreply.github.com>
2020-04-18 22:26:28 +0700

    Update Imunisasi_model.php

--- application/models/Imunisasi_model.php
+++ application/models/Imunisasi_model.php
@@ -3,26 +3,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Imunisasi_model extends CI_Model {
 
-	public function get() {
-		
+	public function get_ims($id_ims) {
+		$this->db->where('id',$id_ims);
+		return $this->db->get('imunisasi')->row_array();
 	}
     
-    public function get_all() {
-		
+    public function get_all_ims() {
+		return $this->db->get('imunisasi')->result_array();
 	}
     
     
-    public function create() {
-		
+    public function create_ims($data) {
+		return $this->db->insert('imunisasi',$data);
 	}
     
-    public function update() {
-		
+    public function update_ims($id_ims, $data) {
+		$this->db->where('id',$id_ims);
+		return $this->db->update('imunisasi',$data);
 	}
     
-    public function delete() {
-		
+    public function delete($id_ims) {
+		$this->db->where('id',$id_ims);
+		return $this->db->delete('imunisasi');
 	}
+    
     
     public function search() {
 		
