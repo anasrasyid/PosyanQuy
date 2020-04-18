43f1266c2ed3156a5d72b50d58f1a2330a870d95
Nisa1999 <48039590+Nisa1999@users.noreply.github.com>
2020-04-18 22:24:54 +0700

    Update Antrian_model.php
    function search masih kosong gan

--- application/models/Antrian_model.php
+++ application/models/Antrian_model.php
@@ -3,25 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Antrian_model extends CI_Model {
 
-	public function get() {
-		
+	public function get_antrian($id_antrian) {
+		$this->db->where('id',$id_antrian);
+		return $this->db->get('antrian')->row_array();
 	}
     
-    public function get_all() {
-		
+    public function get_all_antrian() {
+		return $this->db->get('antrian')->result_array();
 	}
     
     
-    public function create() {
-		
+    public function create_antrian($data) {
+		return $this->db->insert('antrian',$data);
 	}
     
-    public function update() {
-		
+    public function update_antrian($id_antrian,$data) {
+		$this->db->where('id',$id_antrian);
+		return $this->db->update('antrian',$data);
 	}
     
-    public function delete() {
-		
+    public function delete($id_antrian) {
+		$this->db->where('id',$id_antrian);
+		return $this->db->delete('antrian');
 	}
     
     public function search() {
