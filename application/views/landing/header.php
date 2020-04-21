1744845bd29afed2d8a32f39861f12ccb4d4cc7a
anasrasyid <anasrasyid333@gmail.com>
2020-04-21 15:09:24 +0700

    fix view landing page

--- application/views/landing/header.php
+++ application/views/landing/header.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="row"></div></div>
         <nav class="navbar navbar-light bg-white fixed-top shadow p-3 mb-5 bg-white rounded" style="height: 100px;">
-            <a class="navbar-brand"><img src="assets/ic_local_hospital_48px.1.svg" alt="icon"><b>PosyanQuy</b></a>
+            <a class="navbar-brand"><img src="<?php echo base_url('assets/ic_local_hospital_48px.1.svg')?>" alt="icon"><b>PosyanQuy</b></a>
             <ul class="nav justify-content-end">
                 <li class="nav-item">
                   <a class="nav-link active" href="<?php echo site_url('index')?>">Home</a>
@@ -11,7 +11,7 @@
                   <a class="nav-link" href="<?php echo site_url('imunisasi')?>">Imunisasi</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo site_url('article')?>">Article</a>
+                    <a class="nav-link" href="<?php echo site_url('article/0')?>">Article</a>
                   </li>
                 <li class="nav-item">
                   <a class="btn btn-primary" href="<?php echo site_url('login')?>">Login</a>
