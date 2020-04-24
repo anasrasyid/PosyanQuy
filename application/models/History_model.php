89327d4651f09d77c3f577730aba84416acfeb57
Nisa1999 <48039590+Nisa1999@users.noreply.github.com>
2020-04-24 11:44:05 +0700

    Update History_model.php

--- application/models/History_model.php
+++ application/models/History_model.php
@@ -3,11 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class History_model extends CI_Model {
     public function get($id) {
-          
+        $this->db->where('id',$id);
+		return $this->db->get('history')->row();
     }
       
     public function get_all() {
-
+        return $this->db->get('history')->result();
     }
       
     //ini id anak ya parameternya
@@ -17,18 +18,22 @@ class History_model extends CI_Model {
       
       
     public function create($data) {
-
+        return $this->db->insert('history', $data);
     }
       
     public function update($id, $data) {
-
+        $this->db->where('id', $id);
+        return $this->db->update('history', $data);
     }
       
     public function delete($id) {
-
+        $this->db->where('id', $id);
+        return $this->db->delete('history');
     }
       
     public function search($keyword) {
-      
+        $keyword = $this->input->post('keyword', true);
+        $this->db->or_like(['tanggal' => $keyword]);
+        return $this->db->get('history')->result_array();
     }
 }
